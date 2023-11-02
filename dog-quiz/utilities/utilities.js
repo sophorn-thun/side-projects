063f74acfcd4ab2ec89a2829bf2faa5359dfe7be
Sophorn Thun <thunsophorn012@gmail.com>
2023-11-02 09:07:59 +0100

    Adding shuffle array and get multiple choice set functions

--- dog-quiz/utilities/utilities.js
+++ dog-quiz/utilities/utilities.js
@@ -4,3 +4,21 @@ function getRandomItem(array) {
   return array[i];
 }
 
+// Function to shuffle the order of items in array //
+function shuffleArray(array) {
+  return array.sort((a, b) => Math.random() - 0.5)
+}
+
+// Function to get multiple choices including correct answer
+function getMultipleChoice(n, correctAnswer, array) {
+  const choicesSet = [];
+  choicesSet.push(correctAnswer);
+
+  while (choicesSet.length < n) {
+    let selectedChoice = getRandomItem(array);
+    if (choicesSet.indexOf(selectedChoice) < 0) {
+      choicesSet.push(selectedChoice);
+    }
+    return shuffleArray(choicesSet)
+  }
+}
