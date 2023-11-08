4fafb46bcbb3509ae842c2079e4e0de32299104b
Sophorn Thun <thunsophorn012@gmail.com>
2023-11-08 12:11:19 +0100

    install Joi and added schema function for user validation

---
+++ login-app/scripts/user.schema.js
@@ -0,0 +1,8 @@
+import Joi from 'joi'
+
+export const schema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string()
+    .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})'))
+    .required()
+}).unknown(false)
